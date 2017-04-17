cfe3d5c6ee9044344fc15823dc61dd439ea3c50e
Jeff Langr <jeff.langr@outpace.com>
2017-04-17 11:38:02 -0600

    init

---
+++ src/main/java/NumberConverter.java
@@ -0,0 +1,13 @@
+public class NumberConverter {
+    static String[] upToTwenty = {"", "one", "two", "three", "four", "five", "six",
+            "seven", "eight", "nine", "ten", "eleven",
+            "twelve", "thirteen", "fourteen", "fifteen",
+            "sixteen", "seventeen", "eighteen", "nineteen",
+    };
+    static String[] tensDigits = {"", "ten", "twenty", "thirty", "forty",
+            "fifty", "sixty", "seventy", "eighty", "ninety"};
+
+    public static String toWords(int number) {
+        throw new RuntimeException("not yet implemented");
+    }
+}
